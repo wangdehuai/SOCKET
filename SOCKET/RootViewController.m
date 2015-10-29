b0b99bcdcbc9a52969d14a1ec96728b17efeafa8
wangdehuai <wangdehuai@me.com>
2015-10-30 00:21:30 +0800

    add

--- SOCKET/RootViewController.m
+++ SOCKET/RootViewController.m
@@ -23,22 +23,27 @@
 
 -(void)stream:(NSStream *)aStream handleEvent:(NSStreamEvent)eventCode{
     NSLog(@"%@",aStream);
+    //    NSStreamEventOpenCompleted = 1UL << 0,
+    //    NSStreamEventHasBytesAvailable = 1UL << 1,
+    //    NSStreamEventHasSpaceAvailable = 1UL << 2,
+    //    NSStreamEventErrorOccurred = 1UL << 3,
+    //    NSStreamEventEndEncountered = 1UL << 4
     switch (eventCode) {
-        case NSStreamEventOpenCompleted:
+        case NSStreamEventOpenCompleted://数据流打开完成
             NSLog(@"数据流打开完成");
             break;
-        case NSStreamEventHasBytesAvailable:
+        case NSStreamEventHasBytesAvailable://有可读字节
             NSLog(@"有可读字节");
             [self readBytes];
             break;
-        case NSStreamEventHasSpaceAvailable:
+        case NSStreamEventHasSpaceAvailable:// 可发送字节
             NSLog(@"可发送字节");
             break;
-        case NSStreamEventErrorOccurred:
+        case NSStreamEventErrorOccurred://连接错误
             NSLog(@"连接错误");
             break;
-        case NSStreamEventEndEncountered:
-            NSLog(@"到达流末尾，要关闭输入输出流");
+        case NSStreamEventEndEncountered://到达流未尾，要关闭输入输出流
+            NSLog(@"到达流未尾，要关闭输入输出流");
             [outputStream close];
             [inputStream close];
             [outputStream removeFromRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
@@ -53,11 +58,11 @@
     //连接服务器
     CFReadStreamRef readStream;
     CFWriteStreamRef writeStream;
-    CFStreamCreatePairWithSocketToHost(NULL,(__bridge CFStringRef) SOCKET_HOST, SOCKET_PORT, &readStream, &writeStream);
+    CFStreamCreatePairWithSocketToHost(NULL,(__bridge CFStringRef) host, port, &readStream, &writeStream);
     inputStream = (__bridge NSInputStream *)(readStream);
     outputStream = (__bridge NSOutputStream *)(writeStream);
-    [inputStream setDelegate:self];
-    [outputStream setDelegate:self];
+    inputStream.delegate = self;
+    outputStream.delegate = self;
     [inputStream scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     [outputStream scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     [inputStream open];
