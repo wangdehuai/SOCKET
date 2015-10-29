fc9ced475d339dff66ec807ec84240d0d53b811b
wangdehuai <wangdehuai@me.com>
2015-10-29 23:57:52 +0800

    Initial Commit

---
+++ SOCKET/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SOCKET
+//
+//  Created by 王德怀 on 15/10/29.
+//  Copyright © 2015年 王德怀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
